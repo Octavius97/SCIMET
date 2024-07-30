42c091336ec82efc6b88c68efaeaaa9e9dce143b
Octavio Morales <octavius@hotmail.es>
2024-07-30 00:19:07 +0000

    Switch startup form and add new forms and resources
    Changed the startup form from `Form1` to `Main` in `Program.cs`.
    Updated `SCIM Email Templates.csproj` to include new forms and resources:
    - Added `Main Forms\Main.cs` and `Main Forms\Main.Designer.cs`.
    - Added `Main Forms\NewTicket.cs`, `Main Forms\NewTicket.Designer.cs`, and `NewTicket.resx`.
    - Included font files: `CascadiaCode.ttf`, `CascadiaCodePL.ttf`, `CascadiaMono.ttf`, `Montserrat.ttf`.
    
    The new forms `Main` and `NewTicket` are implemented in their respective `.cs` and `.Designer.cs` files, with `NewTicket` also having an associated resource file.

--- SCIM Email Templates/Program.cs
+++ SCIM Email Templates/Program.cs
@@ -16,7 +16,7 @@ namespace SCIM_Email_Templates
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main_Forms.Main());
         }
     }
 }
